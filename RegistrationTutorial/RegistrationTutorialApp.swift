fb7da019b89fafe86465abfad05790f5761553df
akrammotory <93082320+akrammotory@users.noreply.github.com>
2024-07-21 16:42:01 +0300

    Initial Commit

---
+++ RegistrationTutorial/RegistrationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationTutorialApp.swift
+//  RegistrationTutorial
+//
+//  Created by Akram Husseini on 21/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
